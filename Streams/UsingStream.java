e3713b1064ddcc85725caa908aa3a2e5719de391
vikrant chauhan <i.vikrant.me@gmail.com>
2026-06-13 21:29:55 +0530

    java streams , new features

---
+++ Streams/UsingStream.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class UsingStream {
+    public static void main(String[] args) {
+      List<String> fruits = List.of("banana","apple","mango","orange","grapes");
+      
+
+      fruits.forEach((fruit)-> System.out.println(fruit));
+
+    }
+}
