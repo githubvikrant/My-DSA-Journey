14d89bbd2d0d6b1646b49aa0f6b1a511c860ccdb
githubvikrant <157897962+githubvikrant@users.noreply.github.com>
2026-08-09 00:31:40 +0530

    Time: 2 ms (45.1%), Space: 46.3 MB (64.56%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -5,18 +5,18 @@ class Solution {
         vis[0] = true;
         Queue<Integer> q = new LinkedList<>();
         q.add(0);
-        while(q.size()>0){
+        while (!q.isEmpty()) {
             int curr = q.remove();
-            for(int ele: rooms.get(curr)){
-                if(!vis[ele]){
+            for (int ele : rooms.get(curr)) {
+                if (!vis[ele]) {
                     vis[ele] = true;
                     q.add(ele);
                 }
             }
         }
 
-        for(boolean ele: vis){
-            if(!ele){
+        for (boolean ele : vis) {
+            if (!ele) {
                 return false;
             }
         }
