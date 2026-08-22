cdcc76087ed8a5e9bcd2c45a3e4f7a86df993d01
githubvikrant <157897962+githubvikrant@users.noreply.github.com>
2026-08-22 23:27:15 +0530

    Time: 4 ms (10.14%), Space: 268.9 MB (5.33%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -9,7 +9,7 @@ class Solution {
         reverse(nums,k,n-1);
     }
 
-    public static void reverse(int[] arr,int start,int end){
+    public void reverse(int[] arr,int start,int end){
         while(start<end){
             int temp = arr[start];
             arr[start] = arr[end];
